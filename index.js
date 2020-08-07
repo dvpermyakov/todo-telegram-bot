1efefb823e5ae9c80b10eaa30b2ea28608b15c99
dvpermyakov <dvpermyakov1@gmail.com>
2020-08-07 14:38:29 +0400

    update token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const {Telegraf} = require('telegraf')
 
-const bot = new Telegraf("1383903178:AAGAKK4pq03_y1RVxWQ5l_Xpf9JXWoAhvxQ")
+const token = "1383903178:AAGAKK4pq03_y1RVxWQ5l_Xpf9JXWoAhvxQ"
+const bot = new Telegraf(token)
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
